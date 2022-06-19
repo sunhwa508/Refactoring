6a159326aab365dc21adc1648ae908942ed9d3a0
sunhwalee <jamie.dev@kidsnote.com>
2022-06-19 18:26:23 +0900

    refctor: 하나의 함수 쪼개기

--- ch6/6-1.js
+++ ch6/6-1.js
@@ -1,24 +1,34 @@
 export function printOwing(invoice) {
+  printBanner();
+  let outstanding = calculateOutstanding(invoice);
+  recordDueDate(invoice);
+  printDetails(invoice, outstanding);
+}
+
+function printBanner(){
+    console.log('***********************');
+    console.log('**** Customer Owes ****');
+    console.log('***********************');
+}
+
+function calculateOutstanding(invoice){
   let outstanding = 0;
-
-  console.log('***********************');
-  console.log('**** Customer Owes ****');
-  console.log('***********************');
-
-  // calculate outstanding
   for (const o of invoice.orders) {
     outstanding += o.amount;
   }
+  return outstanding;
+}
 
-  // record due date
-  const today = new Date();
-  invoice.dueDate = new Date(
-    today.getFullYear(),
-    today.getMonth(),
-    today.getDate() + 30
-  );
+function recordDueDate(invoice){
+    const today = new Date();
+    invoice.dueDate = new Date(
+      today.getFullYear(),
+      today.getMonth(),
+      today.getDate() + 30
+    );
+}
 
-  //print details
+function printDetails(invoice, outstanding){
   console.log(`name: ${invoice.customer}`);
   console.log(`amount: ${outstanding}`);
   console.log(`due: ${invoice.dueDate.toLocaleDateString()}`);
@@ -28,4 +38,5 @@ const invoice = {
   orders: [{ amount: 2 }, { amount: 5 }],
   customer: '엘리',
 };
+
 printOwing(invoice);
