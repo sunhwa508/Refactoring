136493dac5c7a4fa9d81bce61af31cbc1e32c7c4
sunhwalee <jamie.dev@kidsnote.com>
2022-07-01 09:44:00 +0900

    refac: 변수명 변경

--- ch6/6-5-1.js
+++ ch6/6-5-1.js
@@ -1,3 +1,5 @@
-export function circum(radius) {
+export function circumference(radius) {
   return 2 * Math.PI * radius;
 }
+// F2 누르고 수정하면 호출부도 전부 수정 됨 
+
--- ch6/6-7.js
+++ ch6/6-7.js
@@ -1,6 +1,11 @@
 let a = height * width;
+let area = width * height;
 
 const cpyNm = '애플';
+const companyName = '애플';
 
 let tpHd = '제목없음';
+let title = '제목없음';
+
 let result = `<h1>${tpHd}</h1>`;
+let result2 = `<h1>${title}</h1>`;
