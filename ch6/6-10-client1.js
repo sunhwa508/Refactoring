1bdae33e25a8ac3a2f1c15a89f6de4282eb1614e
Jamie <2017sunhwa@gmail.com>
2022-07-16 20:14:54 +0900

    refac: 함수 쪼개기 깊은 복사 커밋

--- ch6/6-10-client1.js
+++ ch6/6-10-client1.js
@@ -1,6 +1,15 @@
-import { acquireReading, baseRate } from './6-10.js';
+// import { acquireReading, baseRate } from './6-10.js';
 
-const aReading = acquireReading();
+// const aReading = acquireReading();
 
-const baseCharge = baseRate(aReading.month, aReading.year) * aReading.quantity;
-console.log(baseCharge);
+// const baseCharge = baseRate(aReading.month, aReading.year) * aReading.quantity;
+// console.log(baseCharge);
+
+import { acquireReading, enrichReading } from './6-10.js';
+
+const rawReading = acquireReading();
+const reading = enrichReading(rawReading)
+
+console.log(reading.baseCharge);
+console.log(reading.texableCharge);
+
--- ch6/6-10.js
+++ ch6/6-10.js
@@ -1,3 +1,5 @@
+import _ from 'lodash';
+
 const reading = { customer: 'ivan', quantity: 10, month: 5, year: 2017 };
 
 export function acquireReading() {
@@ -7,4 +9,18 @@ export function acquireReading() {
 export function baseRate(month, year) {
   if (year === 2017 && month === 5) return 0.1;
   return 0.2;
+}
+
+export function enrichReading(original){
+  // const result = {...original}; //Object.assign : 얕은복사
+  const result = _.cloneDeep(original);
+  result.baseCharge = calculateBaseCharge(result);
+  result.taxableCharge = Math.max(
+    0, result.baseCharge - taxThreshole(result.year)
+  );
+  return result
+}
+
+function calculateBaseCharge(reading){
+  return baseRate(reading.month, reading.year) * reading.quantity
 }
